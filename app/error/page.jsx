05b6195e70c6c25cb03616910f3b570cc0846931
Brigitte <bgoelersloug@wesleyan.edu>
2024-07-23 22:21:28 -0400

    update error page

--- app/error/page.jsx
+++ app/error/page.jsx
@@ -1,3 +1,14 @@
 export default function ErrorPage() {
-    return <p>Sorry, something went wrong</p>
+    return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
+      <h1 className="text-2xl font-bold mb-4">Sorry, something went wrong</h1>
+      <br></br>
+      <p>Don't have an account already? Make sure to sign up before trying to login.</p>
+      <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2 mt-4"
+          > 
+          <a href="/login"> Login/Signup</a>
+      </button>
+    </main>
+  )
   }
