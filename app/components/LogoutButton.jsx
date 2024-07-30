90336b063e3e55c2f90f5b4975a303f4e808800a
Brigitte <bgoelersloug@wesleyan.edu>
2024-07-30 14:01:52 -0400

    fix navbar

--- app/components/LogoutButton.jsx
+++ app/components/LogoutButton.jsx
@@ -6,6 +6,7 @@ export default function LogoutButton(){
    return (
        <button
        onClick={() => logout()}
-       className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition duration-200 ease-in-out">
-           Logout
+       className="-mr-1 px-2 py-1 bg-red-600 text-white font-medium rounded-lg hover:bg-red-700 transition duration-200 ease-in-out">
+       Logout
        </button>)}
+
--- app/components/LogoutOrLogin.jsx
+++ app/components/LogoutOrLogin.jsx
@@ -13,7 +13,8 @@ export default async function LogoutOrLogin(){
            </div>)}
    return (
        <div>
-           <a className="text-white hover:text-blue-500" href="/login">
+           {/* <a className="text-white hover:text-blue-500" href="/login"> */}
+           <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="/login">
                Login
            </a>
        </div>)}
