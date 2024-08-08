aba3f3db9212edc7b70255be8848fbf263a9eb6a
Brigitte <bgoelersloug@wesleyan.edu>
2024-08-08 12:54:04 -0400

    update homepage for logged in

--- app/page.js
+++ app/page.js
@@ -8,6 +8,16 @@ export default async function Home() {
   const supabase = createClient();
   const {data} = await supabase.auth.getUser();
 
+  if (data.user){
+    return (
+      <main className="bg-gray-800 min-h-screen flex items-center justify-center text-center text-white p-5">
+      <div>
+        <h1 className="text-4xl font-bold mb-12 -mt-24"> Tracker App </h1>
+        <h1 className="text-2xl mb-16"> To log data for whatever you want to track, and to see graphs over time </h1>
+      </div>
+    </main>    
+    )}
+
   return (
     <main className="bg-gray-800 min-h-screen flex items-center justify-center text-center text-white p-5">
       <div>
@@ -19,7 +29,6 @@ export default async function Home() {
         <a href="/login"> Login/Signup</a>
         </button>
       </div>
-      {/* <JokeFetcher user={data.user} /> */}
     </main>
 
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
