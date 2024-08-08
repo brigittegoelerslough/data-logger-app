bf16a348a9c4e1edcd4da2c52b4097439aad8f79
Brigitte <bgoelersloug@wesleyan.edu>
2024-08-08 16:51:30 -0400

    safari css

--- app/components/LineChartSelector.jsx
+++ app/components/LineChartSelector.jsx
@@ -208,7 +208,7 @@ export default function LineChartSelector(things){
             Time Period:
         </h1>
 
-        <select className="max-w-64 m-auto text-black col-span-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        <select className="max-w-64 h-10 m-auto text-black col-span-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             ref={timeRef}
             onChange={async () => {
                 const timescale = timeRef.current.value;
