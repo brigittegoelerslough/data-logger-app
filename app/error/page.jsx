3fe3ce534e8de438621291f75ccc197f7afb8a0d
Brigitte <bgoelersloug@wesleyan.edu>
2024-08-08 13:07:50 -0400

    css error page

--- app/error/page.jsx
+++ app/error/page.jsx
@@ -1,11 +1,11 @@
 export default function ErrorPage() {
     return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white">
-      <h1 className="text-2xl font-bold mb-4">Sorry, something went wrong</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold mb-4 -mt-20 ">Sorry, something went wrong</h1>
       <br></br>
-      <p>Don't have an account already? Make sure to sign up before trying to login.</p>
+      <p className="text-large text-center"> Don't have an account already? Make sure to sign up before trying to login. </p>
       <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2 mt-4"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2 mt-8"
           > 
           <a href="/login"> Login/Signup</a>
       </button>
